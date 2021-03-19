e45ffe34e71054158fcb9bbdd07e61ea8900a734
Tim <acronym1986@gmail.com>
2021-03-19 17:28:47 +1100

    code created and passed test

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numFeet){
+  let fee;
+  
+  if (numFeet <= 400){
+    fee = 'This one is on me!';
+    return fee;
+  }
+    else if ( numFeet > 2000 && numFeet < 2500 ) {  
+    fee = 'I will gladly take your thirty bucks.';
+    return fee;
+  }  
+   else if (numFeet > 2500) {  
+    fee = 'No can do.';
+    return fee;
+  }  
+}
+ 
+
+
+
+
+function ternaryCheckCity(cityName){
   // Write your code here!
+   const destination = (cityName == 'NYC')? 'Ok, sounds good.': 
+                       (cityName == 'Pittsburgh')? 'No go.' :
+                       'more name';
+   return destination;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+   switch (tip){
+     case 'generous':
+           message = 'Thank you so much.';
+           return message;
+           break;
+
+     case 'not as generous':
+           message = 'Thank you.';
+           return message;
+           break;
+
+     case 'thanks for everything':
+           message = 'Bye.'
+           return message;
+           break;
+
+    default:
+           message = 'Sorry, I could not tell';
+           return message;
+
+   }
 }
